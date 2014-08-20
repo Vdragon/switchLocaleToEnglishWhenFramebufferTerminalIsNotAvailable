1ac135d15c456aa0fc1288301eabb78f32dc3612
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-08-20 08:26:36 +0800

    script - convert to function-based model

--- Source_code/switchLocaleToEnglishWhenFramebufferTerminalIsNotAvailable.source.sh
+++ Source_code/switchLocaleToEnglishWhenFramebufferTerminalIsNotAvailable.source.sh
@@ -2,20 +2,29 @@
 # Enable shell debugging
 set -x
 
-# Change software language that supports LANGUAGE environment variable to English
-LANGUAGE=en
-export LANGUAGE
+switchToEnglishLocale(){
+	# Change software language that supports LANGUAGE environment variable to English
+	LANGUAGE=en
+	export LANGUAGE
 
-# Change software language that don't respect LANGUAGE environment variable, but however respects LC_MESSAGES locale to Amercan English
-# note that only LANGAUGE environment variable support two character locale specification
-# LC_MESSAGES=en_US
-LC_MESSAGES=en_US.UTF-8
-export LC_MESSAGES
+	# Change software language that don't respect LANGUAGE environment variable, but however respects LC_MESSAGES locale to Amercan English
+	# note that only LANGAUGE environment variable support two character locale specification
+	# LC_MESSAGES=en_US
+	LC_MESSAGES=en_US.UTF-8
+	export LC_MESSAGES
 
-# Change software language that do not support LANGAUGE but respects LANG environment variable instead
-# LANG=en_US
-LANG=en_US.UTF-8
-export LANG
+	# Change software language that do not support LANGAUGE but respects LANG environment variable instead
+	# LANG=en_US
+	LANG=en_US.UTF-8
+	export LANG
+
+}
+
+main(){
+	#TODO: implement framebuffer device detection
+	switchToEnglishLocale
+}
+main
 
 # Disable shell debugging
 set +x
